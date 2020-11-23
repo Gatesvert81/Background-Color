e097b36a3219effab6ff65f98281830611b9aee0
Gatesvert81 <63714591+Gatesvert81@users.noreply.github.com>
2020-11-23 21:41:31 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var _ = require('lodash');
+
+let array = [1,2,3,4,5,6,7,8]
+console.log('answer:', _.without(array, 3));
+
+let css = document.querySelector("h3")
+let color1 = document.querySelector(".color1")
+let color2 = document.querySelector(".color2")
+let body = document.getElementById("gradient")
+let h3 = document.getElementById("h3")
+
+function backgroundColor() {
+  body.style.background = "linear-gradient(to right, "
+  + color1.value
+  +", "
+  +color2.value
+  + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", backgroundColor)
+
+color2.addEventListener("input", backgroundColor)
